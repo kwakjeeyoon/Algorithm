7bc6949119742930f434a8925e307b74dbe1c26d
kwakjeeyoon <jeeyoon744@naver.com>
2021-08-16 20:44:55 +0900

    8_16 [프로그래머스]도둑질 공부

--- DP/[프로그래머스] 도둑질 - 다른 답.py
+++ DP/[프로그래머스] 도둑질 - 다른 답.py
@@ -5,6 +5,8 @@ def solution(a):
     for i in a[3:]:
         x1, y1, z1 = y1, z1, max(x1, y1)+i
         x2, y2, z2 = y2, z2, max(x2, y2)+i
+        # 여기서 x1,y1에 먼저 y1,z1이 들어가고 max(y1,z1)이 연산되는것 처럼 보이지만
+        # 기존의 max(x1,y1)이 연산되어 들어가게 된다.
     return max(x1, y1, y2, z2)
 
-print(solution([1, 2, 3, 1]	))
+print(solution([1,3,5,6,3,2,7,6]))
--- DP/[프로그래머스] 도둑질.py
+++ DP/[프로그래머스] 도둑질.py
@@ -1,9 +1,10 @@
 def solution(money):
     dp = [0]*len(money)
     dp[0] = money[0]
-    dp[1] = max(dp[0], money[1])
-    for i in range(2,len(money)-1):
-        dp[i] = max(dp[i-1], dp[i-2]+money[i])
+    # 첫번째 집은 무조건 터는 경우 -> 마지막 집은 무조건 못턴다
+    dp[1] = max(dp[0], money[1]) # 1번 인덱스는 i-2가 없기 때문에 0 과 1중 큰 수를 넣는다
+    for i in range(2,len(money)-1): # range에서 마지막 집은 무조건 0으로 비워두기
+        dp[i] = max(dp[i-1], dp[i-2]+money[i]) # i-1(이전 집)과 i-2+i(자기자신) 중에 큰것 선택
     dp2 = [0] * len(money)
     dp2[1] = money[1]
     dp2[2] = max(money[1], money[2])
